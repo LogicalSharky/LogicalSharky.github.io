d17589e0a843f1ff42fd916b34f0644885e099e2
LogicalSharky <mats.pinket@gmail.com>
2025-10-13 09:25:51 +0200

    new + old site

---
+++ plantpicker/clipboard.js
@@ -0,0 +1,29 @@
+const allTags = {
+  "TYPE OF PLANT": ["Tree","Pine tree","Shrub","Multi-stemmed tree","Bamboo","Ornamental grass","Perennial","Biennial","Climbing plant","Ground cover","Water plant","Flower bulb","Weeds"],
+  "USE": ["Hedge","Thickets","Wadi","Coastal area","species for birds","species for insects","Fragrant","Hardening tolerant","Monkey proof","Pioneer species","Houseplant","Roof garden","Drought tolerant"],
+  "FLOWER COLOUR": ["Red flowers","Orange flowers","Yellow flowers","White flowers","Pink flowers","Purple flowers","Blue flowers","Black flowers","Brown flowers","Green flowers"],
+  "LEAF COLOUR": ["Red leaves","Orange leaves","Yellow leaves","White leaves","Pink leaves","Purple leaves","Blue leaves","Black leaves","Brown leaves","Green leaves","Grey leaves"],
+  "FLOWERING PERIOD": ["January","February","March","April","May","June","July","August","September","October","November","December"],
+  "HEIGHT": [">0.2m","0.2m","0.5m","1m","1.5m","2m","3m","5m","7-8m","10m","20m","+20m"],
+  "SUNLIGHT": ["Full sunlight","Half shade","Shade"],
+  "GROUND TYPE": ["Clay","Loam","Sandy soil","Sand","Rocks"],
+  "MOISTURE LEVEL": ["Moist","Well-drained","Dry"],
+  "PH VALUE":["Acid","Neutral","Lime"],
+  "LOCATION":["Windscheltered"],
+  "WINTER FEATURES": ["Evergreen","Winter features","No winterfeatures"],
+  "AUTUMN COLOURS": ["Red autumn leaves","Yellow autumn leaves"],
+  "FRUITS": ["Berries","Nuts","fruits","Pine cones","Catkins","Seed pods"],
+  "EDIBLE": ["Berries","Nuts","Leaves","Flowers","Stem","Fruits","after processing"],
+  "ORIGIN": ["Native","Introduced","Invasive"],
+  "SPECIAL FEATURES":["Toxic","Luminescent","Tropical look","Large leaves","Thorns"],
+  "PLANTING DENSITY":["10-12/m²","8-9/m²","6-7/m²","4-5/m²","2-3/m²","1/m²"],
+  "AVERAGE PRICE P9 OR 10/12":["€5","€10","€15","€20","€25","€30","€40","€50","€75","€100","€150","€200","€300","€500","€750","€1000","+€1000"],
+  "SCHOOLYEAR": ["1st","2nd"],
+  "OTHER": ["text"]
+};
+
+// To do: 
+// - 3. Remove the old plant entries - 1. Sort tags AZ - 2. Add every tag I can think of - 4. Enter the plants I have photo's of - 5. search bar text 
+
+
+
